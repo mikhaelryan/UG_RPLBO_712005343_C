46a01961bba592a07f53a3eda3facc0d2463f918
mikhaelryan <80173365+mikhaelryan@users.noreply.github.com>
2022-02-23 17:50:41 +0700

    Add files via upload

---
+++ UG3-71200534/2_71200534/com/ug3/soal2/UG3soal2.java
@@ -0,0 +1,13 @@
+package com.ug3.soal1;
+import java.util.Scanner;
+
+ class UG3soal2 {
+     public static void main(String args[]) {
+         System.out.println("Bilangan Fibonanci dari kata");
+         Scanner inp1 = new Scanner(System.in);
+         System.out.println("Masukan input pertama: ");
+         String input1 = inp1.nextLine();
+         Scanner inp2 = new Scanner(System.in);
+         System.out.println("Masukan input kedua: ");
+         String input2 = inp2.nextLine();
+}
