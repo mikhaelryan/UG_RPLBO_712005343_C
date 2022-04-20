6ec05c3bbb0d2700fe96750e2ca626bde0419225
mikhaelryan <80173365+mikhaelryan@users.noreply.github.com>
2022-04-20 18:52:07 +0700

    Add files via upload

--- UG8C_71200534/src/main/java/org/example/App.java
+++ UG8C_71200534/src/main/java/org/example/App.java
@@ -5,6 +5,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        
+        Lina a = new Lina();
+        DragonKnight b= new DragonKnight();
+        LegionCommander c = new LegionCommander();
+
+        a.skill(b);
+
+        b.skill();
+        b.attack(a);
+        b.attack(a);
+
+        c.skill(a);
+
+        a.skill(c);
+
+        a.showCharacterInfo();
+        b.showCharacterInfo();
+        c.showCharacterInfo();
+
+
     }
 }
